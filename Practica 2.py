ad26d01d300dc24d8b5e4d7cca7ea70a83b2b7e1
JuanDmp27 <113940487+JuanDmp27@users.noreply.github.com>
2022-11-01 02:59:55 -0400

    Delete Practica 2.py

--- Practica 2.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("ingrese el primer numero"))
-num2 = int(input("ingrese el segundo numero"))
-from math import sqrt
-
-Resultado = int(sqrt(num1*num1 + num2*num2))
-
-
-print("El resultado del ejercicios de %s" %(Resultado)) 
